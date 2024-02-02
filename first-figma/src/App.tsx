91ca8d40c699a74b2254800f9a9b98c1d8b6d82d
Antonio Klenitsky <a_klenitsky@mail.ru>
2024-02-02 11:23:31 +0300

    add

---
+++ first-figma/src/App.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css';
+import {Card} from "./components/card/Card";
+
+function App() {
+    return (
+        <section>
+
+            <Card title="Hello" text="Hellloooooo"/>
+
+        </section>
+    );
+}
+
+export default App;
+
+
+
+// СДЕЛАТЬ MAPом возможность добавления карточек без ctrlc ctrlv
